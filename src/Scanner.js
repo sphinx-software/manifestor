d091f538ae55da1e3c5378a42b33d31512eed906
rikky <son.levuthai@gmail.com>
2018-01-22 11:50:50 +0700

    fix syntax error

--- src/Scanner.js
+++ src/Scanner.js
@@ -4,7 +4,7 @@ const path      = require('path');
 class Scanner {
 
     async scan(directory, ignores = []) {
-        ignores.push((file, stats) => stats.isFile() && !path.extname(file).match(/\.js$/);
+        ignores.push((file, stats) => stats.isFile() && !path.extname(file).match(/\.js$/));
 
         return await recursive(directory, ignores);
     }
